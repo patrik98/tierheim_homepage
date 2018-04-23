fe51bae137339378045cf41f21889442c1031b3e
patrik <golecpatrik@gmail.com>
2018-04-23 15:39:44 +0200

    databasedump update

--- includes/views/register.php
+++ includes/views/register.php
@@ -59,21 +59,21 @@ echo $this->header;
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="email">E-Mail:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="email" class="form-control" id="email" name="email" placeholder="E-Mail eingeben" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="E-Mail eingeben" maxlength="60" required>
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="pwd">Passwort:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Passwort eingeben" required>
+                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Passwort eingeben" maxlength="255" required>
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="pwd2">Passwort wiederholen:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="password" class="form-control" id="pwd2" name="pwd2" placeholder="Passwort wiederholen" required>
+                <input type="password" class="form-control" id="pwd2" name="pwd2" placeholder="Passwort wiederholen" maxlength="255" required>
             </div>
         </div>
 
@@ -84,14 +84,14 @@ echo $this->header;
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="vname">Vorname:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="vname" name="vname" placeholder="Vorname" required>
+                <input type="text" class="form-control" id="vname" name="vname" placeholder="Vorname" maxlength="100" required>
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="nname">Nachname:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="nname" name="nname" placeholder="Nachname" required>
+                <input type="text" class="form-control" id="nname" name="nname" placeholder="Nachname" maxlength="100" required>
             </div>
         </div>
 
@@ -105,28 +105,28 @@ echo $this->header;
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="strasse">Straße:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="strasse" name="strasse" placeholder="Straße">
+                <input type="text" class="form-control" id="strasse" name="strasse" placeholder="Straße" maxlength="50">
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="hausnr">Hausnr.:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="hausnr" name="hausnr" placeholder="Hausnr.">
+                <input type="text" class="form-control" id="hausnr" name="hausnr" placeholder="Hausnr." maxlength="10">
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="plz">Postleitzahl:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="plz" name="plz" placeholder="PLZ">
+                <input type="text" class="form-control" id="plz" name="plz" placeholder="PLZ" maxlength="15">
             </div>
         </div>
 
         <div class="form-group">
             <label class="col-xs-12 col-md-2" for="ort">Ort:</label>
             <div class="col-xs-12 col-md-10">
-                <input type="text" class="form-control" id="ort" name="ort" placeholder="Ort">
+                <input type="text" class="form-control" id="ort" name="ort" placeholder="Ort" maxlength="50">
             </div>
         </div>
 
