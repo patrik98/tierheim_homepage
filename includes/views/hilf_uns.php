9705d8bab4cf72d1edd761f4d20c976a80e508f3
OlGe404 <oliver.geyer@hotmail.de>
2018-05-04 16:36:26 +0200

    hilf_uns.php fertig, Überschriften + Text zentrierung eingefügt

--- includes/views/hilf_uns.php
+++ includes/views/hilf_uns.php
@@ -12,14 +12,37 @@
     </div>
 
     <div class="row">
-        <div class="col-xs-12 col-md-8"></div>
-        <div class="col-xs-12 col-md-4">
-            <img class="img-responsive" src="images/600x600.jpg" />
+        <div class="col-xs-0 col-md-1 col-lg-2"></div>
+        <div class="col-xs-12 col-md-10 col-lg-8">
+                <div class="row">
+                    <div class="col-xs-7">
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
+
+Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer</p></div>
+                    <div class="col-xs-5"><img class="img-responsive" src="images/600x600.jpg" /></div>
+                </div>
         </div>
-
+        <div class="col-xs-0 col-md-1 col-lg-2"></div>
     </div>
-    <hr/>
 
+    <div class="row">
+    <div class="col-xs-0 col-md-1 col-lg-2"></div>
+        <div class="col-xs-12 col-md-10 col-lg-8">
+            <h2>Spendenkonto</h2>
+            <p>commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
+
+Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer</p>
+
+        <button type="button" class="btn btn-default">Via Paypal</button>
+        <button type="button" class="btn btn-default">Sofortüberweisung</button>
+        <button type="button" class="btn btn-default">Kreditkarte</button>
+        </div>
+        <div class="col-xs-0 col-md-1 col-lg-2"></div>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
--- includes/views/kontakt.php
+++ includes/views/kontakt.php
@@ -10,7 +10,8 @@
     <div class="col-xs-0 col-md-1 col-lg-2"></div>
 </div>
     <div class="row">
-        <div class="col-xs-12">
+    <div class="col-xs-0 col-md-1 col-lg-2"></div>
+        <div class="col-xs-12 col-md-10 col-lg-8">
             <h2>Tierheim "abc"</h2>
             <address>
                 <strong>Adresse:</strong><br>
@@ -22,12 +23,13 @@
                 <i class ="glyphicon glyphicon-phone"></i> 01234 567 890<br>
                 <i class ="glyphicon glyphicon-envelope"></i> kontakt@tierheimabc.com
             </address>
-        </div>
-        <div class="col-xs-12">
+            
+            <!-- Google-Maps Ansicht -->
             <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2691.1766005352965!2d12.171479215411974!3d47.583806979183144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4776375b00761d89%3A0x7b6312b5d056ce32!2sFH+Kufstein+Tirol!5e0!3m2!1sde!2sat!4v1524562727467">
             </iframe>
         </div>
+        <div class="col-xs-0 col-md-1 col-lg-2"></div>
     </div>
 </div><!-- /.container-fluid -->
 <?php echo $this->footer; ?>
