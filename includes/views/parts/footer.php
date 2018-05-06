c80f7be9492c6cf6d9e82e75754926087c8ab45e
OlGe404 <oliver.geyer@hotmail.de>
2018-05-06 14:35:24 +0200

    Überarbeitung Footer

--- includes/views/parts/footer.php
+++ includes/views/parts/footer.php
@@ -1,21 +1,53 @@
 <footer>
     <div id="footer">
         <div class="container">
+            <!-- Kontakt, Impressum, AGBs, Geschäftsbedingungen -->
             <div class="row">
-                        <ul>
-                            <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">Kontakt</a> </li>
-                            <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">Impressum</a> </li>
-                            <li class="col-sm-3 col-xs-12"><a href="https://de.wikipedia.org/wiki/Haftungsklausel" target="_blank">Haftungsausschluss</a>
-                            </li>
-                            <li class="col-sm-3 col-xs-12"><a href="https://de.wikipedia.org/wiki/Allgemeine_Gesch%C3%A4ftsbedingungen" target="_blank">AGB</a>
-                            </li>
-                        </ul>
+                <ul>
+                    <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">Kontakt</a> </li>
+                    <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">Impressum</a> </li>
+                    <li class="col-sm-3 col-xs-12"><a href="https://de.wikipedia.org/wiki/Haftungsklausel" target="_blank">Haftungsausschluss</a>
+                    </li>
+                    <li class="col-sm-3 col-xs-12"><a href="https://de.wikipedia.org/wiki/Allgemeine_Gesch%C3%A4ftsbedingungen" target="_blank">AGB</a>
+                    </li>
+                </ul>
             </div>
-                <div class="nav navbar-nav">
-                            <a href="http://www.youtube.com/" target="_blank"><img src="images/youtube.png" width="45px"></a>
-                            <a href="http://www.facebook.com/" target="_blank"><img src="images/facebook.png" width="45px"></a>
-                            <a href="http://www.twitter.com/" target="_blank"><img src="images/twitter.png" width="45px"></a>
+
+            <!-- Abstandshalter -->
+            <div class="row">
+                <div class="col-xs-0 col-sm-12"><hr/ style="margin-top:15px; border-color: black"></div>
+            </div>
+
+            <!-- Social Media -->
+            <div class="row">
+                <div class="col-sm-4">
                 </div>
+                <div class="col-sm-4">
+                    <ul>
+                        <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">
+                            <a href="http://www.youtube.com/" target="_blank">
+                                <img src="images/youtube.png" width="45px">
+                            </a>
+                        </li>
+                        <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">
+                            <a href="http://www.facebook.com/" target="_blank">
+                                <img src="images/facebook.png" width="45px">
+                            </a>
+                        </li>
+                        <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">
+                            <a href="http://www.twitter.com/" target="_blank">
+                                <img src="images/twitter.png" width="45px">
+                            </a>
+                        </li>
+                        <li class="col-sm-3 col-xs-12"><a href="kontakt" target="_blank">
+                            <a href="http://www.instagram.com/" target="_blank">
+                                <img src="images/instagram.png" width="45px">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
         </div>
     </div>
 </footer>
